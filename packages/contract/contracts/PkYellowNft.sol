02300a78c5fc7286a8abbf87da5ed8416c94678e
Filipe Oliveira <hi@fmoliveira.dev>
2021-09-26 18:49:52 -0300

    Add starter level to generated image

--- packages/contract/contracts/PkYellowNft.sol
+++ packages/contract/contracts/PkYellowNft.sol
@@ -17,9 +17,11 @@ contract PkYellowNft is ERC721URIStorage {
 	uint256 public publicIssued = 0;
 
 	string baseSvg =
-		'<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: black; font-family: sans-serif; font-size: 20px; font-weight: bold; text-transform: uppercase; }</style><rect width="100%" height="100%" fill="white" stroke="';
+		'<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: black; font-family: sans-serif; font-size: 18px; } .bold { font-size: 20px; font-weight: bold; text-transform: uppercase; }</style><rect width="100%" height="100%" fill="white" stroke="';
 	string moreSvg =
-		'" stroke-width="30" /><text x="50%" y="75%" class="base" dominant-baseline="middle" text-anchor="middle">';
+		'" stroke-width="30" /><text x="50%" y="75%" class="base bold" dominant-baseline="middle" text-anchor="middle">';
+	string levelSvg =
+		'</text><text x="50%" y="82%" class="base" dominant-baseline="middle" text-anchor="middle">';
 	string imageSvg = '</text><image href=" ';
 	string endSvg =
 		'" height="100" width="100" transform="translate(125,125)"/></svg>';
@@ -64,6 +66,7 @@ contract PkYellowNft is ERC721URIStorage {
 		uint256 newItemId = _tokenIds.current();
 
 		uint256 starterIndex = pickStarterPokemon(newItemId);
+		uint256 starterLevel = pickStarterLevel(newItemId);
 
 		string memory svg = string(
 			abi.encodePacked(
@@ -71,6 +74,8 @@ contract PkYellowNft is ERC721URIStorage {
 				colors[starterIndex],
 				moreSvg,
 				starters[starterIndex],
+				levelSvg,
+				starterLevel,
 				imageSvg,
 				images[starterIndex],
 				endSvg
@@ -121,11 +126,24 @@ contract PkYellowNft is ERC721URIStorage {
 			string(
 				abi.encodePacked(
 					"STARTER",
-					msg.sender,
+					block.timestamp,
 					Strings.toString(tokenId)
 				)
 			)
 		);
 		return rand % starters.length;
+	}
+
+	function pickStarterLevel(uint256 tokenId) private view returns (uint256) {
+		uint256 rand = random(
+			string(
+				abi.encodePacked(
+					"LEVEL",
+					block.timestamp,
+					Strings.toString(tokenId)
+				)
+			)
+		);
+		return (rand % 80) + 5;
 	}
 }
