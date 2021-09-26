79213ac11f7925a7d8686f76cbe408dcf337c7bf
Filipe Oliveira <hi@fmoliveira.dev>
2021-09-26 12:56:58 -0300

    Add extra file with pokemon types

--- scripts/fetch-attributes.mjs
+++ scripts/fetch-attributes.mjs
@@ -1,9 +1,12 @@
 import fs from "fs";
+import path from "path";
+import { exec } from "child_process";
 import fetch from "node-fetch";
 
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
 
 const POKEMON_MAX = 151;
+const DATA_FOLDER = path.resolve(process.cwd(), "data");
 
 async function fetchAttributes() {
   const fetching = new Array(POKEMON_MAX).fill(0).map((_, index) => {
@@ -18,11 +21,15 @@ async function fetchAttributes() {
     types: pk.types.map((t) => t.type.name),
     moves: pk.moves.map((m) => m.move.name).slice(0, 10),
   }));
+  const allTypes = [...new Set(attributes.flatMap((pk) => pk.types))].sort();
 
   fs.writeFileSync(
-    "../data/pokemon-attributes.json",
+    `${DATA_FOLDER}/pokemon-attributes.json`,
     JSON.stringify(attributes)
   );
+  fs.writeFileSync(`${DATA_FOLDER}/all-types.json`, JSON.stringify(allTypes));
+
+  exec(`npx prettier -w ${DATA_FOLDER}/*.json`);
 }
 
 fetchAttributes();
